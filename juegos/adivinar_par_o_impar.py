20c1db72f93469bcc347fd4c739101031c15b8f6
isiaburto <163420116+isiaburto@users.noreply.github.com>
2024-03-14 18:53:08 +0000

    ARREGLAMOS PAR O IMPAR

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -4,9 +4,9 @@ def adivinar_par_o_impar():
     par = False
     if n%2 == 0:
         par = True
-    print(n, "es par o impar?")
+    print("Es par o impar?")
     a = input("Ingrese su respuesta: ")
     if (a == "par" and par) or (a == "impar" and par == False):
-        print("Adivinaste! Felicitaciones!")
-        return("")
-    print("Fallaste, ;(")    
+        print("Adivinaste!, el numero era",n,"Felicitaciones!")
+    else:
+        print("Fallaste, el numero era",n,";(")    
